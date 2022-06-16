d5632ac33a1d7dd1a7cb9fb98675e525b0d2447b
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-16 18:10:39 +0200

    add homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-
+<nav class="navbar navbar-expand navbar-light bg-light d-flex justify-content-center">
+    <ul class="nav navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="{{route('welcome')}}">Home <span class="visually-hidden">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{route('comics.index')}}">Comics</a>
+        </li>
+    </ul>
+</nav>
 @endsection
