f3a4b0900630fd44de053b1f8bcea2ee9974d462
Alexxboc <boccardi.alessandro@gmail.com>
2022-06-16 15:51:16 +0200

    First commit:
    - Install npm dependecies
    - Install bootstrap
    - Use bootstrap in app.scss file
    - Create new layout
    - Create partials for footer and header
    - Create welcome page

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer id="site_footer">
+    <div class="copyright">
+        Made with &hearts; by Alexx
+    </div>
+    <!-- /.copyright -->
+</footer>
+<!-- /#site_footer -->
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header id="site_header">
+    <div class="logo">
+        LOGO
+    </div>
+    <!-- /.logo -->
+</header>
+<!-- /#site_header -->
